89480c4a7e897b10c91958d94ce11316237bf20f
Lei Xia <fifthwolf@qq.com>
2018-02-23 16:30:05 +0800

    增加递归创建目录功能

--- images_down.py
+++ images_down.py
@@ -1,8 +1,19 @@
 # -*- coding: UTF-8 -*-
-import cgi, urllib, re
+import cgi, urllib, re, os
 import public
 
 print("Content-Type: text/html\n")
+
+def mkdir(path):
+    if not os.access(path, os.R_OK):
+        path_last = len(path) - 1
+        if path[path_last] == '/' or path[path_last] == '\\':
+            path = path[0: path_last]
+
+        mkdir(os.path.dirname(path))
+
+        if not os.path.isfile(path):
+            os.mkdir(path, 0755)
 
 info = public.GetValue(cgi.FieldStorage())
 
@@ -12,6 +23,8 @@ path = info.get('path')[0]
 
 if path[len(path) - 1] != '/':
     path = path + '/'
+
+mkdir(path)
 
 for img in images:
     file = re.findall(r"\/([^/]+)(\.\w+$)", img)
@@ -24,3 +37,4 @@ for img in images:
     urllib.urlretrieve(img, path + str(file[0][0]) + str(file[0][1]))
 
 print 1
+
